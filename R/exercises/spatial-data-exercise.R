0e03694ded0e57a0e7bf938a7ba7d1374a57e4b4
Kira McDonald <kira.mcd.99@gmail.com>
2023-11-22 10:04:22 -0500

    added more comments and explanation to the spatial-data-exercise from 11/21 class.

--- R/exercises/spatial-data-exercise.R
+++ R/exercises/spatial-data-exercise.R
@@ -1,12 +1,13 @@
 library(tidyverse)
-# install.packages('sf')
 
 # the package `sf` will underlie nearly all spatial work in R!
+# install.packages('sf')
 library(sf)
 
 
 # the package `tigris` allows us to quickly pull geometries from the census
 # bureau
+#install.packages('tigris')
 cos <- tigris::counties(state = 'NY', year = 2021)
 
 # we can notice that the spatial data is the data.frame class AND the `sf` class
@@ -27,8 +28,6 @@ cos %>%
 
 # coordinate reference systems (CRS) --------------------------------------------
 
-#
-
 # st_crs asks what coordinate reference system, or map projection, the data is
 # using
 cos %>% st_crs()
@@ -38,21 +37,21 @@ cos %>%
   st_transform(4326) %>%
   st_crs()
 
-
-
 # to load a shapefile -----------------------------------------------------
 
+#' this was downloaded from NYC OpenData as a shapefile:
+#'
+#' https://data.cityofnewyork.us/City-Government/Borough-Boundaries/tqmj-j8zm
 ddir <- 'local-data/Borough Boundaries/'
 fn <- list.files(ddir, pattern = 'shp')
 
 boros <- paste0(ddir, fn) %>%
   st_read()
 
-
+# mapview is very useful for quick, interactive maps: the `zcol` argument should
+# be a column in the data that will correspond to the fill color for the map.
 library(mapview)
 boros %>% mapview(zcol = 'boro_name')
-
-
 
 
 # example tidycensus pull -------------------------------------------------
@@ -60,21 +59,32 @@ boros %>% mapview(zcol = 'boro_name')
 
 #' tidycensus vignette:
 #' https://walker-data.com/tidycensus/articles/basic-usage.html
-#'
-#' (we will need the API key)
 
 # install.packages('tidycensus')
-install.packages('tigris')
-
 acs.vars <-
   tidycensus::load_variables(
      2021
     ,dataset = 'acs5'
   )
 
+# (it starts out looking slightly messy, with exclamation points instead of
+# spaces, but we can clean up pretty quickly)
+acs.vars <- acs.vars %>%
+  mutate(label = # replaces the double-! with space
+           gsub('!!', ' ', label)) %>%
+  mutate(label = # remove trailing colons (`$` means the "end of the string" in regex)
+           gsub(':$', '', label)
+         )
 
+# I have this saved on my computer so i can quickly look at the available ACS
+# tables:
 acs.vars
+#acs.vars %>% write.csv(file = '~/acs-variables.csv')
 
+# to query median household income -- we can identify that the variable
+# "B19013_001" corresponds with median income from the variable list we just
+# pulled, or we could google it or look on websites like censusreporter.com
+# (https://censusreporter.org/tables/B19013/)
 med.hh.inc <-
   tidycensus::get_acs(
      geography = 'tract'
@@ -85,6 +95,9 @@ med.hh.inc <-
     ,geometry = T
   )
 
+# to make some quick maps!
+
+# static map:
 med.hh.inc %>%
   ggplot(
     aes(fill = estimate)
@@ -94,6 +107,5 @@ med.hh.inc %>%
     name = 'Median household income'
   )
 
-
-
+# interactive map:
 med.hh.inc %>% mapview(zcol = 'estimate')
