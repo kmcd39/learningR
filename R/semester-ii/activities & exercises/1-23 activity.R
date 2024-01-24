e9a2324f265f68266a61cc3de2f45d2917e366f5
Kira McDonald <kira.mcd.99@gmail.com>
2024-01-23 20:47:53 -0500

    end of 1-23 class

--- R/semester-ii/activities & exercises/1-23 activity.R
+++ R/semester-ii/activities & exercises/1-23 activity.R
@@ -1,14 +1,26 @@
 library(tidyverse)
 library(sf)
+options(tigris_use_cache=TRUE)
 
 rm(list = ls())
 
+
+# to make sure we have the packages installed: ---------------------------------
+
+# (we only have to install once, until we want to update or are on a different
+# computer)
+
+# install.packages('tidycensus')
+# install.packages('tigris')
+# install.packages('sf')
+
 # get median income by borough --------------------------------------------
 
+# get NY counties (areas and name)
 ny.cos <- tigris::counties(state =36, year = 2021) %>%
   rename_with(tolower)
 
-
+# get NY neighborhood-level income
 med.hh.incs <-
   tidycensus::get_acs(
     geography = 'tract'
@@ -23,9 +35,6 @@ med.hh.incs
 
 # let's just use the 5 boros of NYC for simplicity -- we can use Regex to
 # filter!
-boros.regx <- 'Kings|Queens|^New York|Richmond|Bronx'
-#visaux::nyboros
-
 ny.inc <-
   med.hh.incs %>%
     select(-name) %>%
@@ -38,4 +47,60 @@ ny.inc <-
               ,by = c('county.fips' = 'countyfp'))
 
 ny.inc
+
+
+
+# activity! ---------------------------------------------------------------
+
+# let's see how many nbhds are in each boro -- mostly to check that we have the
+# boros we expect.
+ny.inc %>%
+  count(name)
+
+#' next let's make a histogram of neighborhood incomes in NYC
+#'
+
+ny.inc %>%
+  ggplot(
+    aes(
+      x = estimate
+      )
+  ) +
+  geom_histogram(
+    fill = '#008888'
+    ,binwidth = 5000
+  )
+
+
+## what about the breakdown by borough? ------------------------------------
+
+ny.inc %>%
+  ggplot(
+    aes(
+      x = estimate
+    )
+  ) +
+  geom_histogram(
+    fill = '#008888'
+    ,binwidth = 5000
+  ) +
+  facet_wrap(
+    vars(name)
+    ,ncol = 1
+  )
+
+
+ny.inc %>%
+  ggplot(
+    aes(
+      x = estimate
+      ,fill = name
+    )
+  ) +
+  geom_histogram(
+    binwidth = 5000
+  )
+
+
+
 
