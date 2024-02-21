b629094019ce161fab21db29ec9a4f3c64d2018c
Kira McDonald <kira.mcd.99@gmail.com>
2024-02-20 20:02:09 -0500

    basic ggplot added

--- R/semester-ii/activities & exercises/2-20 activity.R
+++ R/semester-ii/activities & exercises/2-20 activity.R
@@ -6,3 +6,17 @@ pth <- 'https://raw.githubusercontent.com/kmcd39/learningR/main/local-data/proce
 eia <- read.csv(pth) %>% tibble()
 
 eia %>% count(description)
+
+eia %>% glimpse()
+
+eia %>% count(description, lbl, is.tot)
+
+eia %>%
+  filter(!is.tot) %>%
+  ggplot(
+    aes(x = year
+        , y = value
+        ,color = lbl)
+  ) +
+  geom_line()
+
